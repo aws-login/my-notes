502c822d0d151dfbe1f5e25777b4b1b1bc2e861b
aws-login <aws.login28@gmail.com>
2026-05-10 13:01:16 +0000

    new topic added ci/cd

---
+++ sidebar.js
@@ -0,0 +1,10 @@
+// sidebar.js
+const items = document.querySelectorAll('.sidebar li');
+
+items.forEach(item => {
+  item.addEventListener('click', () => {
+    items.forEach(i => i.classList.remove('active'));
+    item.classList.add('active');
+  });
+});
+
